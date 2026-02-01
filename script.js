b00d09872cf9665e1f3ea5abcb0140f1fd89fd7b
Sander <hosin8388@gmail.com>
2026-02-01 23:34:36 +0330

    completed the New Arrivals part

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const triggerOpen = document.querySelectorAll('[trigger-button]');
 const triggerClose = document.querySelectorAll('[close-button ]');
 const overlay = document.querySelector('[data-overlay]');
 
-for(let i = 0; i< triggerOpen.length; i++){
+for (let i = 0; i < triggerOpen.length; i++) {
     let currentId = triggerOpen[i].dataset.target,
-    targetEl = document.querySelector(`#${currentId}`)
+        targetEl = document.querySelector(`#${currentId}`)
 
     const openData = function () {
         targetEl.classList.remove('active');
@@ -17,7 +17,7 @@ for(let i = 0; i< triggerOpen.length; i++){
 
     targetEl.querySelector('[close-button]').addEventListener('click', openData);
     overlay.addEventListener('click', openData);
-    
+
 }
 
 // mobile-menu submenu
@@ -25,7 +25,7 @@ const submenu = document.querySelectorAll('.child-trigger');
 submenu.forEach((menu) => menu.addEventListener('click', function (e) {
     e.preventDefault();
     submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('active') : null);
-    if(this.closest('.has-child').classList != 'active'){
+    if (this.closest('.has-child').classList != 'active') {
         this.closest('.has-child').classList.toggle('active');
     }
 }))
@@ -34,14 +34,47 @@ submenu.forEach((menu) => menu.addEventListener('click', function (e) {
 
 const swiper = new Swiper('.sliderbox', {
 
-  loop: true,
-  effect: 'fade',
-  autoHeight: true,
+    loop: true,
+    effect: 'fade',
+    autoHeight: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+});
+
+// Carousel
+
+const carousel = new Swiper('.carouselbox', {
+
+    spaceBetween: 30,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+
+    // If we need navigation
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        481: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+            centeredSlides: false,
+        },
+        640: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+            centeredSlides: false,
+        },
+        992: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            centeredSlides: false,
+        },
+    }
 
 });
