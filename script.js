1bafeee8f9e895141a0daae787f366a224dd62e9
Sander <hosin8388@gmail.com>
2026-03-02 13:15:32 +0330

    working on the Product Images

--- script.js
+++ script.js
@@ -131,3 +131,32 @@ const carousel = new Swiper('.carouselbox', {
     }
 
 });
+
+// Product Image > Product Page :
+
+const thumbImage = new Swiper('.thumbnail-image', {
+
+    // loop: true,
+    direction: 'vertical',
+    spaceBetween: 15,
+    slidesPerView: 1,
+    freeMode : true,
+    watchSlidesProgress: true,  
+
+});
+
+const mainImage = new Swiper('.main-image', {
+
+    loop: true,
+    autoHeight: true,
+    
+    pagination : {
+        el: '.swiper-pagination',
+        clickable : true,
+    },
+
+    thumbs: {
+        swiper: thumbImage
+    },
+
+});
